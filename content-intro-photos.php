85c3ff1c622fc790f9bc840bee282d619f70422c
Brian Watson <bswatson@gmail.com>
2014-06-10 02:35:00 -0400

    Replacing theme image content

--- content-intro-photos.php
+++ content-intro-photos.php
@@ -5,22 +5,22 @@
 ?>
 	<div id="intro-wrap">
 		<div id="intro" class="preload darken" data-autoplay="5000" data-navigation="true" data-pagination="true" data-transition="fade">
-			<div class="intro-item" style="background-image: url(http://placehold.it/1800x600/ddd/fff&text=Beetle%20image);">
+			<div class="intro-item" style="background-image: url(http://placehold.it/1800x600/ddd/fff&text=bsw.io);">
 				<div class="photocaption">
 					<h4>Photography by <a href="http://danielezedda.com/">Daniele Zedda</a></h4>
 				</div><!-- photocaption -->
 			</div>
-			<div class="intro-item" style="background-image: url(http://placehold.it/1800x600/ddd/fff&text=Beetle%20image);">
+			<div class="intro-item" style="background-image: url(http://placehold.it/1800x600/ddd/fff&text=bsw.io);">
 				<div class="photocaption">
 					<h4>Photography by <a href="http://danielezedda.com/">Daniele Zedda</a></h4>
 				</div><!-- photocaption -->
 			</div>	
-			<div class="intro-item" style="background-image: url(http://placehold.it/1800x600/ddd/fff&text=Beetle%20image);">
+			<div class="intro-item" style="background-image: url(http://placehold.it/1800x600/ddd/fff&text=bsw.io);">
 				<div class="photocaption">
 					<h4>Photography by <a href="http://danielezedda.com/">Daniele Zedda</a></h4>
 				</div><!-- photocaption -->
 			</div>	
-			<div class="intro-item" style="background-image: url(http://placehold.it/1800x600/ddd/fff&text=Beetle%20image);">
+			<div class="intro-item" style="background-image: url(http://placehold.it/1800x600/ddd/fff&text=bsw.io);">
 				<div class="photocaption">
 					<h4>Photography by <a href="http://danielezedda.com/">Daniele Zedda</a></h4>
 				</div><!-- photocaption -->
